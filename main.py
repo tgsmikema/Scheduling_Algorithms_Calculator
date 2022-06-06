1bb63340de5f4e5df03e27a3feb4921c086d4e67
tgsmikema <32582220+tgsmikema@users.noreply.github.com>
2022-06-06 13:58:33 +0800

    Finalise Main function for user selection, only few selected function applied

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import round_robin
 import pre_emptive_short_job_first
 from EDF_earliest_deadline_first import EDF
 from SCT_shortest_comp_time import SCT
-from queue import PriorityQueue
 
-
-
-# first_come_first_serve.FCFS()
-
-# round_robin.RR()
-
-pre_emptive_short_job_first.PE_SJF()
-
-# EDF()
-
-# SCT()
-
+print("PLEASE enter which Algo you would like to use: \n "
+      "FCFS (first come first serve) ; RR (round robin) ; SJF (preemptive shortest job first) \n"
+      "EDF (Earliest deadline first) ; SCT (shortest compute time) :\n")
+input_str = input()
+if input_str == "FCFS":
+    first_come_first_serve.FCFS()
+elif input_str == "RR":
+    round_robin.RR()
+elif input_str == "SJF":
+    pre_emptive_short_job_first.PE_SJF()
+elif input_str == "EDF":
+    EDF()
+elif input_str == "SCT":
+    SCT()
+else:
+    print("WRONG INPUT!!!!!!")
